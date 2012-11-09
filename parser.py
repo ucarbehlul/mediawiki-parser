566d221ffe6aae832a77ae6c31fbf8a7d21a0762
Peter Potrowl <peter017@gmail.com>
2012-11-09 11:08:30 +0100

    Fix bug related to CR/LF

--- parser.py
+++ parser.py
@@ -65,7 +65,10 @@ tree = parser.parse(preprocessed_text.leaves())
 
 output = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-<head><title>Test!</title></head>""" + tree.leaves() + "</html>"
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="type" content="text/html; charset=utf-8" />
+	<title>Test!</title></head>""" + tree.leaves() + "</html>"
 
 file("article.htm", "w").write(output.encode('UTF-8'))
 
